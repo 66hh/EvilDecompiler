8ef4e1e5f3dedbe425c06a5591c137b011b60956
66hh <49398720+66hh@users.noreply.github.com>
2025-09-02 16:23:59 +0800

    删除无用依赖

--- EvilDecompiler.ByteCode/Operand/IQuickJsOperand.cs
+++ EvilDecompiler.ByteCode/Operand/IQuickJsOperand.cs
@@ -1,11 +1,4 @@
-﻿using EvilDecompiler.ByteCode.Type;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EvilDecompiler.ByteCode.Operand
+﻿namespace EvilDecompiler.ByteCode.Operand
 {
     public interface IQuickJsOperand
     {
