09b15dc778e83015a9d84e96b579755d1f5721ef
66hh <49398720+66hh@users.noreply.github.com>
2025-09-02 13:58:49 +0800

    初始化项目

---
+++ EvilDecompiler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EvilDecompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
